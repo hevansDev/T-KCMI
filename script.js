abf32d2093b90a29199e319c0c081600841893d3
HughEvans01 <hugh.evans@dae.mn>
2022-06-23 15:58:21 +0100

    Added the ability to answer questions as well as the scoring function

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-var question = 0;
+var question = 1;
 
-var questions = JSON.parse(questions);
+var answers = new Array(30);
+
+var results = {"competing":0,
+                "collaborating":0,
+                "compromising":0,
+                "avoiding":0,
+                "accommodating":0};
+
+var questions = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': 'questions.json',
+        'dataType': 'json',
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+
 setQuestion();
 
 
 function setQuestion() {
-    document.querySelector('input[name="questionForm"]').checked=false;
-    document.getElementById("answerA").innerHTML=questions[question].A;
-    document.getElementById("answerB").innerHTML=questions[question].B;
+
+    document.querySelector('input[id="optionA"]').checked=false;
+    document.querySelector('input[id="optionB"]').checked=false;
+    if (answers[question] != undefined) {
+        document.querySelector('input[id='+answers[question]+']').checked=true;
+    }
+    
+    document.getElementById("answerA").innerHTML=questions[question].A.text;
+    document.getElementById("answerB").innerHTML=questions[question].B.text;
+    document.getElementById("question").innerHTML=question+"/30";
 }
 
 function getAnswer() {
-    if (document.querySelector('input[name="questionForm"]').checked) {
+    if ((document.querySelector('input[id="optionA"]').checked)|(document.querySelector('input[id="optionB"]').checked)) {
+        answers[question-1] = document.querySelector('input[name="questionForm"]:checked').value;
         return true;
     }
     return false;
@@ -19,10 +48,13 @@ function getAnswer() {
 
 function nextQuestion() {
     if (getAnswer()){
-        if (question < questions.length-1) {
+        if (question < 30) {
             question++;
+            setQuestion();
+        } else {
+            getResults();
         }
-        setQuestion();
+
     }
 }
 
@@ -33,3 +65,13 @@ function prevQuestion() {
     setQuestion();
 }
 
+function getResults() {
+    console.log(answers.length);
+    for (i=0; i < 30; i++) {
+        q = questions[i+1];
+        a = answers[i];
+        v = q[a].type;
+        results[v]+=1
+    }
+    console.log(results)
+}
