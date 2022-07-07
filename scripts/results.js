d2b9c445630785b8e52861b04d3ad148e66a965c
HughEvans01 <hugh.evans@dae.mn>
2022-07-07 12:20:03 +0100

    Made changes Pippa suggested

--- scripts/results.js
+++ scripts/results.js
@@ -1,7 +1,53 @@
-var results = JSON.parse(localStorage.getItem("results"));
+function validateAnswers() {
+    var valid = true;
+    for (i=0; i < 30; i++) {
+        var number = i + 1;
+        var A = document.getElementById("radio"+number+"A").checked;
+        var B = document.getElementById("radio"+number+"B").checked;
+        if (!(A || B)) {
+            valid = false;
+            document.getElementById("radio"+number+"A").focus();
+            break;
+        }
+    }
+    if (valid) {getResults();};
+}
 
-document.getElementById("competing").innerHTML+=results["competing"];
-document.getElementById("collaborating").innerHTML+=results["collaborating"];
-document.getElementById("compromising").innerHTML+=results["compromising"];
-document.getElementById("avoiding").innerHTML+=results["avoiding"];
-document.getElementById("accommodating").innerHTML+=results["accommodating"];
+function getResults() {
+    var results = {"competing":0,
+                   "collaborating":0,
+                   "compromising":0,
+                   "avoiding":0,
+                   "accommodating":0};
+    for (i=0; i < 30; i++) {
+        var number = i + 1;
+        var A = document.getElementById("radio"+number+"A");
+        var B = document.getElementById("radio"+number+"B");
+
+        var answer;
+        if (A.checked) {
+            answer = A;
+        } else if (B.checked) {
+            answer = B;
+        }
+        results[answer.value] += 1;
+    }
+    displayResults(results);
+}
+
+function displayResults(results) {
+    document.getElementById("competing").innerHTML= "Competing "+results["competing"];
+    document.getElementById("collaborating").innerHTML="Collaborating " +results["collaborating"];
+    document.getElementById("compromising").innerHTML="Compromising "+results["compromising"];
+    document.getElementById("avoiding").innerHTML="Avoiding "+results["avoiding"];
+    document.getElementById("accommodating").innerHTML="Accommodating "+results["accommodating"];
+
+}
+
+
+function setAnswers() {
+    for (i=0; i < 30; i++) {
+        var number = i + 1;
+        var A = document.getElementById("radio"+number+"A").checked = true;
+    }
+}
